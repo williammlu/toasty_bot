790331a84f37caa0bf8bafc36dca5fa241bc8a51
William Lu <wml@berkeley.edu>
2018-04-11 13:38:34 +1300

    Adding starter scripts

---
+++ scripts/rotate.py
@@ -0,0 +1,36 @@
+from time import sleep
+import RPi.GPIO as GPIO
+
+DIR = 20   # Direction GPIO Pin
+STEP = 21  # Step GPIO Pin
+CW = 1     # Clockwise Rotation
+CCW = 0    # Counterclockwise Rotation
+SPR = 48   # Steps per Revolution (360 / 7.5)
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(DIR, GPIO.OUT)
+GPIO.setup(STEP, GPIO.OUT)
+GPIO.output(DIR, CW)
+
+step_count = SPR
+delay = .0208
+
+print("step 1")
+for x in range(step_count):
+    GPIO.output(STEP, GPIO.HIGH)
+    sleep(delay)
+    GPIO.output(STEP, GPIO.LOW)
+    sleep(delay)
+    print("Cycle {}", x)
+print("step 2")
+
+sleep(.5)
+GPIO.output(DIR, CCW)
+for x in range(step_count):
+    GPIO.output(STEP, GPIO.HIGH)
+    sleep(delay)
+    GPIO.output(STEP, GPIO.LOW)
+    sleep(delay)
+
+print("step 3")
+GPIO.cleanup()
