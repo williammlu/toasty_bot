62314a835084f61bf6c06d39ab9756f71a18236c
William Lu <wml@berkeley.edu>
2018-04-22 12:53:18 +1300

    Update to pigpio instead of GPIO lib

--- run/button_test.py
+++ run/button_test.py
@@ -1,12 +1,29 @@
 import RPi.GPIO as GPIO
 import time
 
-GPIO.setmode(GPIO.BCM)
+# GPIO.setmode(GPIO.BCM)
+# 
+# GPIO.setup(20, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+# 
+# while True:
+    # input_state = GPIO.input(20)
+    # if input_state == 1:
+        # print('Button Pressed')
+        # time.sleep(0.2)
+# 
 
-GPIO.setup(20, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+import pigpio
 
+pin = 20
+
+pi = pigpio.pi()
+
+pi.set_mode(pin, pigpio.INPUT)
+pi.set_pull_up_down(pin, pigpio.PUD_DOWN)
 while True:
-    input_state = GPIO.input(20)
+    input_state = pi.read(pin)
     if input_state == 1:
         print('Button Pressed')
         time.sleep(0.2)
+
+
