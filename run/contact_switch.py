c98333b0fb2db3b224628712f7d7545c3039c887
William Lu <wml@berkeley.edu>
2018-04-22 15:20:20 +1300

    Adding bounce time for contact switch class

--- run/contact_switch.py
+++ run/contact_switch.py
@@ -4,7 +4,7 @@ import pigpio
 
 
 class ContactSwitch:
-    def __init__(self, pin, callback=None, bounce_ms=200):
+    def __init__(self, pin, callback=None, bounce_ms=0.1):
         self.pin = pin 
         pi = pigpio.pi() 
         pi.set_mode(pin, pigpio.INPUT)
@@ -13,23 +13,13 @@ class ContactSwitch:
 
 
         def bounce_wrapper_callback(gpio, level, tick):
-            
-            print( time.time() - self.last_activation)
             if bounce_ms < time.time() - self.last_activation:
-                print("i'm in")
                 self.last_activation = time.time()
+                print("calling callback") 
                 if callback:
                     callback(gpio, level, tick)
-            # print(gpio, level, tick)
         
         pi.callback(pin, pigpio.EITHER_EDGE, bounce_wrapper_callback)
-
-        # while True:
-            # time.sleep(0.2)
-            # input_state = pi.read(pin)
-            # if input_state == 1:
-                # print('Button Pressed')
-                # time.sleep(0.2)
 
 c = ContactSwitch(20)
 while True:
