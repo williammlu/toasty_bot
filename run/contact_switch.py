1ea1c2156393d63b4ec590a7f11fc629c45e3a82
William Lu <wml@berkeley.edu>
2018-04-23 11:55:04 +1300

    Adding support for 3 steppers in run.py

--- run/contact_switch.py
+++ run/contact_switch.py
@@ -10,17 +10,22 @@ class ContactSwitch:
         pi.set_mode(pin, pigpio.INPUT)
         pi.set_pull_up_down(pin, pigpio.PUD_DOWN)
         self.last_activation = time.time()
+        self.callback = callback
 
 
         def bounce_wrapper_callback(gpio, level, tick):
             if bounce_ms < time.time() - self.last_activation:
                 self.last_activation = time.time()
                 print("calling callback") 
-                if callback:
-                    callback(gpio, level, tick)
+                if self.callback:
+                    self.callback(gpio, level, tick)
         
-        pi.callback(pin, pigpio.EITHER_EDGE, bounce_wrapper_callback)
+        pi.callback(pin, pigpio.RISING_EDGE, bounce_wrapper_callback)
+    
+    def is_pressed(self):
+        pi = pigpio.pi()
+        return pi.read(pin)
 
-c = ContactSwitch(20)
-while True:
-    time.sleep(0.2)
+# c = ContactSwitch(20)
+# while True:
+    # time.sleep(0.2)
--- run/run.py
+++ run/run.py
@@ -1,6 +1,8 @@
 # from motor2 import Motor2
 from motor import StepperMotor
 from motor import GearBoxMotor
+from contact_switch import ContactSwitch
+
 import pigpio
 import time
 
@@ -8,7 +10,37 @@ import time
 m1 = StepperMotor(13,06, resolution="1/32") # white wires
 m2 = StepperMotor(26,19) # gray and blue wires
 m3 = GearBoxMotor(23) # blue single wire
+m4 = StepperMotor(16,12) # red and gray wire
 
+c1 = ContactSwitch(20)
+
+def stop_all():
+    m1.stop()
+    m2.stop()
+    m3.stop()
+    m4.stop()
+try:
+    print("Starting all motors\nLooping and waiting for contact switch")
+    need_stop =  0
+    def motor_stop_callback(gpio, level, tick):
+        global need_stop
+        if gpio == 20:
+            stop_all()
+            need_stop = 1
+    c1.callback = motor_stop_callback
+
+    for l in range(30):
+        m1.start(duty=25)
+        m2.start(duty=25)
+        m4.start(duty=25)
+        while not need_stop:
+            time.sleep(0.1)
+        need_stop = 0
+        time.sleep(1)
+
+except KeyboardInterrupt:
+    print("keyboard escape")
+    stop_all()
 
 try:
     print("Starting gearbox motor")
@@ -29,29 +61,33 @@ try:
     for l in range(30):
         for k in range(30):
             m1.start(duty=25)
+            m2.start(duty=25)
+            m4.start(duty=25)
             time.sleep(1)
-            m1.stop()
+            stop_all()
             m1.spin(1, 5)
+            m2.spin(1, 5)
+            m4.spin(1, 5)
             time.sleep(1)
 except KeyboardInterrupt:
     print("keyboard escape")
-    m1.stop()
+    stop_all()
     
 try:
     print("Starting quarter rotations")
     for k in range(30):
         m1.spin(0.25,1,True)
+        m2.spin(0.25,1,True)
+        m4.spin(0.25,1,True)
         time.sleep(1)
         m1.spin(0.25,1,False)
+        m2.spin(0.25,1,False)
+        m4.spin(0.25,1,False)
         time.sleep(1)
-        # m2.spin(1 ,2,True)
-        # time.sleep(1)
-        # m2.spin(1,2,False)
-        # time.sleep(1)
         print("cycle", k)
 except KeyboardInterrupt:
     print("keyboard escape")
-    m1.stop()
+    stop_all()
 
 # i = 0
 # while True:
@@ -67,6 +103,9 @@ except KeyboardInterrupt:
 m1.exit()
 m2.exit()
 m3.exit()
+m4.exit()
+pi.stop()
+
 
 
 
